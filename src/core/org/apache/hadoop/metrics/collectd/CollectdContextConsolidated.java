85cd9581e13a844bcc64660628b0313c3fdda939
james.kim <james.kim@nexr.com>
2011-10-17 14:02:46 +0900

    adding contracted collectd rrd

--- src/core/org/apache/hadoop/metrics/collectd/CollectdContextConsolidated.java
+++ src/core/org/apache/hadoop/metrics/collectd/CollectdContextConsolidated.java
@@ -319,6 +319,7 @@ public class CollectdContextConsolidated extends AbstractMetricsContext {
             if (!dirty) {
                 continue;
             }
+            LOG.info("dispatchConsolidated:"       + typedbkey + ", vl:" + vl);
             vl.setPlugin(CollectdContextConsolidated.PLUGIN + "_" + typedbkey);
             vl.setValues(values);
             vl.setType(typedbkey);
