c42215aee1d73858632cab6f7cc380d71158193a
myminseok <james.kim@nexr.com>
2011-10-18 13:08:25 +0900

    hadoopenv

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,3 +2,7 @@ git pull origin master
 ant
 cp build/hadoop-collectd.jar /home/nexr/nexr_platforms/meerkat_backend/hadoop-0.21.0
 
+/home/nexr/nexr_platforms/meerkat_backend/hadoop-0.21.0/bin/stop-all.sh
+sleep 1
+/home/nexr/nexr_platforms/meerkat_backend/hadoop-0.21.0/bin/start-all.sh
+
